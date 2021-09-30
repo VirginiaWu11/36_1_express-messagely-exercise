82c9e6d7d37c9595305e259a1ab6ccc148d5b7a0
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-30 19:57:50 -0400

    add messages

--- data.sql
+++ data.sql
@@ -29,3 +29,19 @@ INSERT INTO users
 INSERT INTO users
   (username, password, first_name, last_name, phone, join_at, last_login_at)
     VALUES ('KK', 'password', 'Hello', 'admin', '111-222-3333', '2021-09-30T23:01:42.102Z','2021-09-30T23:01:42.102Z');
+
+INSERT INTO users
+  (username, password, first_name, last_name, phone, join_at, last_login_at)
+    VALUES ('BB', 'password', 'Bloop', 'Bleep', '111-222-3333', '2021-09-30T23:01:42.102Z','2021-09-30T23:01:42.102Z');
+
+INSERT INTO messages 
+    (from_username, to_username, body, sent_at, read_at)
+    VALUES ('JJ', 'BB', 'hey', '2021-09-30T23:02:33.102Z', '2021-09-30T23:02:35.102Z');
+
+INSERT INTO messages 
+    (from_username, to_username, body, sent_at, read_at)
+    VALUES ('JJ', 'BB', 'hi', '2021-09-30T23:02:34.102Z', '2021-09-30T23:02:35.102Z');
+
+INSERT INTO messages 
+    (from_username, to_username, body, sent_at, read_at)
+    VALUES ('JJ', 'BB', 'u there?', '2021-09-30T23:02:33.102Z', '2021-09-30T23:02:35.102Z');
