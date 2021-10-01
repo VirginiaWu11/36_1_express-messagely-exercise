51c3adbae7294647a05ba4bb5c3e09be7c6055fc
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-30 20:58:21 -0400

    add login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const router = new express.Router();
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError");
 
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+router.get("/login", async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            throw new ExpressError("Username and password required", 400);
+        }
+        const results = await db.query(
+            `SELECT username, password
+            FROM users
+            WHERE username = $1`,
+            [username]
+        );
+        const user = results.rows[0];
+        if (user) {
+            if (await bcrypt.compare(password, user.password)) {
+                return res.json({ message: `Logged in!` });
+            }
+        }
+        throw new ExpressError("Invalid username/password", 400);
+    } catch (e) {
+        next(e);
+    }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
